1134e2f851956d6d4aba08c91c76f60c2102f17a
bbiasi <brennerbiasiss@gmail.com>
2019-07-19 21:11:07 -0300

    Script

--- rs.R
+++ rs.R
@@ -1,4 +1,4 @@
-setwd("D:/R/Residuos_Solidos/30_06_19")
+setwd("D:/R/Project/Residuos_Solidos/30_06_19/rs_artigo")
 
 # SNIS - Série Histórica
 # Sistema Nacional de Informações sobre Saneamento
@@ -14,28 +14,54 @@ setwd("D:/R/Residuos_Solidos/30_06_19")
 # https://imgur.com/APMiMdK
 # https://imgur.com/LuG98Kg
 
+# Glossario
+# http://snis.gov.br/glossarios
+
+# RDO = residuos solidos domiciliares
+# RPU = residuos solidos urbanos
+# RSS = residuos de saude
+# RCC = residuos de construcao civil
+# RIN = residuos industriais
+# RPO = resíduos dos serviços de podas de arvores
+# SLU = limpeza urbana publica (servico, superintendencia, etc.)
+
 # importando dados
 library(readxl)
 FluxoResiduos <- readxl::read_excel("FluxoResiduos.xlsx")
 
-# visualizando NA
+# visualizando NA ----
 library(naniar)
 naniar::gg_miss_var(FluxoResiduos)
 naniar::vis_miss(FluxoResiduos)
 
+# ----
 library(dplyr)
 library(tibble)
-df <- FluxoResiduos %>% 
-  tibble::rowid_to_column()
+library(ggplot2)
 
-df1 <- df %>% 
-  dplyr::filter(Município == "Alagoinhas")
+Bahia <- FluxoResiduos %>% 
+  tibble::rowid_to_column() %>% 
+  dplyr::filter(Estado == "BA") %>% 
+  dplyr::select(-c(`Código do Município`, Estado)) %>% 
+  dplyr::rename("Domiciliar_urbano" = `UP007 - Quantidade de RDO e RPU recebida na unidade de processamento`,
+                "Saude" = `UP008 - Quantidade de RSS recebida na unidade de processamento`,
+                "Industrial" = `UP009 - Quantidade de RIN recebida na unidade de processamento`,
+                "Const_civil" = `UP010 - Quantidade de RCC recebida na unidade de processamento`,
+                "Outros" = `UP011 - Quantidade de outros tipos de resíduos recebida na unidade de processamento`,
+                "Municipio" = Município,
+                "Ano" = `Ano de Referência`,
+                "Municipio_origem" = `UP025 - Municípios de origem dos resíduos`)
 
-df2 <- df %>% 
-  dplyr::filter(`UP025 - Municípios de origem dos resíduos` == "Alagoinhas/BA")
+Alagoinhas <- Bahia %>% 
+  dplyr::filter(Municipio == "Alagoinhas" | Municipio_origem == "Alagoinhas/BA")
 
-df3 <- rbind(df1, df2)
-
-df3 <- df3 %>% 
-  dplyr::filter(! duplicated(rowid))
+Bahia %>% 
+  na.omit() %>% 
+  ggplot2::ggplot() +
+  geom_boxplot(aes(x = Ano, 
+                   y = Domiciliar_urbano,
+                   fill = `UP003 - Tipo de unidade`)) +
+  facet_wrap(~`UP004 - Operador da unidade`, scales = "free") +
+  theme_classic() +
+  theme(axis.text.x = element_blank())
 
