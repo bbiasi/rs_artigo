61e96313c301b6b8638b9f3c5ec0ad6eb8ed7f96
bbiasi <brennerbiasiss@gmail.com>
2019-07-20 00:48:17 -0300

    Script

--- rs.R
+++ rs.R
@@ -24,6 +24,7 @@ setwd("D:/R/Project/Residuos_Solidos/30_06_19/rs_artigo")
 # RIN = residuos industriais
 # RPO = resíduos dos serviços de podas de arvores
 # SLU = limpeza urbana publica (servico, superintendencia, etc.)
+# ATT = area de transbordo e triagem
 
 # importando dados
 library(readxl)
@@ -38,6 +39,7 @@ naniar::vis_miss(FluxoResiduos)
 library(dplyr)
 library(tibble)
 library(ggplot2)
+library(forcats)
 
 Bahia <- FluxoResiduos %>% 
   tibble::rowid_to_column() %>% 
@@ -50,18 +52,71 @@ Bahia <- FluxoResiduos %>%
                 "Outros" = `UP011 - Quantidade de outros tipos de resíduos recebida na unidade de processamento`,
                 "Municipio" = Município,
                 "Ano" = `Ano de Referência`,
-                "Municipio_origem" = `UP025 - Municípios de origem dos resíduos`)
+                "Municipio_origem" = `UP025 - Municípios de origem dos resíduos`,
+                "Op_Unidade" = `UP004 - Operador da unidade`,
+                "Nome_und" = `Nome da Unidade`,
+                "Tipo_und" = `UP003 - Tipo de unidade`,
+                "poda" = `UP067 - Quantidade de RPO recebida na unidade de processamento`,
+                "QTD_receb_municpio_und_proc"= `UP080 - Quantidade total de resíduos recebida na unidade de processamento por cada município`) %>% 
+  dplyr::mutate(Municipio = as.factor(Municipio),
+                Unidades = as.factor(Unidades),
+                Ano = as.factor(Ano),
+                Op_Unidade = as.factor(Op_Unidade),
+                Nome_und = as.factor(Nome_und),
+                Municipio_origem = as.factor(Municipio_origem),
+                Tipo_und = as.factor(Tipo_und)) %>%
+  dplyr::mutate(Tipo_und = forcats::fct_recode(Tipo_und,
+                      att_rcc_vol = "Área de transb e triagem de RCC e volumosos (=ATT)",
+                      aterro = "Aterro controlado",
+                      aterro_rcc = "Aterro de Resíduos da Construção Civil (=inertes)",
+                      aterro = "Aterro sanitário",
+                      autoclave = "Unid. tratamento por microondas ou autoclave",
+                      compostagem = "Unidade de compostagem (pátio ou usina)",
+                      galhos_podas = "Unidade de manejo de galhadas e podas",
+                      transbordo = "Unidade de transbordo",
+                      incineracao = "Unidade de tratamento por incineração",
+                      triagem = "Unidade de triagem (galpão ou usina)",
+                      vala_rss = "Vala especifica de RSS"),
+                Op_Unidade = forcats::fct_recode(Op_Unidade,
+                                                 catadores = "Associação de catadores",
+                                                 emp_priv  = "Empresa privada",
+                                                 municipal = "Prefeitura ou SLU"))
 
-Alagoinhas <- Bahia %>% 
-  dplyr::filter(Municipio == "Alagoinhas" | Municipio_origem == "Alagoinhas/BA")
+levels(Bahia$Op_Unidade)
 
 Bahia %>% 
   na.omit() %>% 
   ggplot2::ggplot() +
-  geom_boxplot(aes(x = Ano, 
-                   y = Domiciliar_urbano,
-                   fill = `UP003 - Tipo de unidade`)) +
-  facet_wrap(~`UP004 - Operador da unidade`, scales = "free") +
+  geom_bar(aes(x = Op_Unidade, 
+               y = Domiciliar_urbano,
+               fill = Tipo_und),
+           stat = "identity") +
+  facet_wrap(~Ano, scales = "free") +
   theme_classic() +
-  theme(axis.text.x = element_blank())
+  theme(axis.text.x = element_text(angle = 90))
+
+Alagoinhas <- Bahia %>% 
+  dplyr::filter(Municipio == "Alagoinhas" | Municipio_origem == "Alagoinhas/BA")
+
+Alagoinhas %>% 
+  na.omit() %>% 
+  ggplot2::ggplot() +
+  geom_bar(aes(x = Op_Unidade, 
+               y = Domiciliar_urbano,
+               fill = Tipo_und),
+           stat = "identity") +
+  facet_wrap(~Ano, scales = "free") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90))
+
+Alagoinhas %>% 
+  na.omit() %>% 
+  dplyr::filter(Tipo_und == "aterro") %>% 
+  ggplot2::ggplot() +
+  geom_bar(aes(x = Ano, 
+               y = Domiciliar_urbano,
+               fill = Op_Unidade),
+           stat = "identity") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90))
 
