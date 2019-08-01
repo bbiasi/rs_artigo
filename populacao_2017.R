efb98f2592de1cd163ed6c57c9087f742f841af2
bbiasi <brennerbiasiss@gmail.com>
2019-07-31 22:07:29 -0300

    população 2017

--- populacao_2017.R
+++ populacao_2017.R
@@ -4,9 +4,7 @@ library(readxl)
 pop_2017 <- readxl::read_excel("D:/R/Project/Residuos_Solidos/30_06_19/rs_artigo/IBGE/estimativa_dou_2017.xlsx", 
                                sheet = "Municípios", skip = 1)
 
-
 # cleaner ----
-
 library(dplyr)
 levels(pop_2017$UF)
 
@@ -20,7 +18,7 @@ df <- df %>%
   dplyr::rename("Municipio" = `NOME DO MUNICÍPIO`,
                 "Pop_est_2017"  = `POPULAÇÃO ESTIMADA`) %>% 
   dplyr::select(Municipio, Pop_est_2017)
-
+# View(df)
 
 # save ----
-
+write.table(df, file = "Pop_est_2017.txt", sep = "")
