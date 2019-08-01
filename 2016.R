3d15a74edf730c16da11c37514e252a7033a6478
bbiasi <brennerbiasiss@gmail.com>
2019-08-01 00:08:10 -0300

    2016

--- 2016.R
+++ 2016.R
@@ -6,9 +6,9 @@ pop_2016 <- readxl::read_excel("IBGE/estimativa_dou_2016_20160913.xlsx",
 
 # cleaner ----
 library(dplyr)
-levels(pop_2017$UF)
+levels(pop_2016$UF)
 
-df <- pop_2017 %>% 
+df <- pop_2016 %>% 
   dplyr::mutate(UF = as.factor(UF))
 
 levels(df$UF)
@@ -16,8 +16,8 @@ levels(df$UF)
 df <- df %>% 
   dplyr::filter(UF == "BA") %>% 
   dplyr::rename("Municipio" = `NOME DO MUNICÍPIO`,
-                "Pop_est_2017"  = `POPULAÇÃO ESTIMADA`) %>% 
-  dplyr::select(Municipio, Pop_est_2017)
+                "Pop_est_2016"  = `POPULAÇÃO ESTIMADA`) %>% 
+  dplyr::select(Municipio, Pop_est_2016)
 # View(df)
 
 # save ----
