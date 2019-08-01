81a2d63a04fdac0665fb0f12115faf0cbf7258c4
bbiasi <brennerbiasiss@gmail.com>
2019-07-31 23:54:07 -0300

    2016

---
+++ 2016.R
@@ -0,0 +1,24 @@
+setwd("D:/R/Project/Residuos_Solidos/30_06_19/rs_artigo")
+
+library(readxl)
+pop_2016 <- readxl::read_excel("IBGE/estimativa_dou_2016_20160913.xlsx", 
+                               sheet = "Municípios", skip = 2)
+
+# cleaner ----
+library(dplyr)
+levels(pop_2017$UF)
+
+df <- pop_2017 %>% 
+  dplyr::mutate(UF = as.factor(UF))
+
+levels(df$UF)
+
+df <- df %>% 
+  dplyr::filter(UF == "BA") %>% 
+  dplyr::rename("Municipio" = `NOME DO MUNICÍPIO`,
+                "Pop_est_2017"  = `POPULAÇÃO ESTIMADA`) %>% 
+  dplyr::select(Municipio, Pop_est_2017)
+# View(df)
+
+# save ----
+write.table(df, file = "2016.txt", sep = "")
